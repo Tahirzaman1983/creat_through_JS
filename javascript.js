5d3872c6e51e00e70c75497b191b7998d052b1ca
Tahir Zaman <167541229+Tahirzaman1983@users.noreply.github.com>
2024-11-21 23:12:17 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,66 @@
+// Print Even number 
+// function add(){
+//     let a=+prompt("enter the first number ");
+//     let b=+prompt("enter the second number");
+//     alert("the sum of a and b " + (a+b));
+// }
+
+
+
+// Print even Number 
+// function add (){
+//     let number=+prompt("enter the number ");
+//         let count=0;
+//     for (let i=1; i<=number; i++){
+//         if(number%i==0){
+//            count ++;
+//         }
+//     }
+//     if(count==2){
+//         alert("the number is prime ");
+//     }
+//     else{
+//         alert("the number is composite ");
+//     }
+// }
+
+
+// reverse the pelindron 
+// function add(){
+//     let a=prompt("enter some thing ");
+//    let revesreName = a.split('').reverse().join('')
+//     alert("The number is reversed "+revesreName)
+
+// }
+
+// //converted to upperCase 
+// function add(){
+//     let a = prompt("enter the name ");
+//     let b = a.toUpperCase();
+//     alert("Your enter statment is converted is upperCase "+b);
+// }
+
+
+
+// print 5 number in arry and find smallest, greatest and average of these numbers . 
+// let arr=[
+//  +prompt("enter the 1st number"),
+//  +prompt("enter the 2nd number"),
+//  +prompt("enter the 3rd number"),
+//  +prompt("enter the 4t number"),
+//  +prompt("enter the 5th number")
+// ];
+// alert("Your number is stored in arry "+arr);
+
+// let small=Math.min(...arr);
+// alert("the smallest number is "+small);
+
+// let great=Math.max(...arr);
+// alert("the greatest number is "+great);
+
+// let sum=0;
+// for (i=0; i<arr.length; i++){
+//     sum=sum+arr[i];
+// }
+// let average=sum/arr.length;
+// alert("the average are these numbers are" + " " +average);
